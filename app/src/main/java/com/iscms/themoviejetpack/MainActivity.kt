410f81f301d2d3f584dfb2580ce5b5a0f79c9773
Leay15 <ivan_yael_gp@outlook.com>
2024-03-19 17:08:07 -0600

    Updates project with latest compile SDK version

--- app/src/main/java/com/iscms/themoviejetpack/MainActivity.kt
+++ app/src/main/java/com/iscms/themoviejetpack/MainActivity.kt
@@ -3,6 +3,7 @@ package com.iscms.themoviejetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -41,6 +42,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TheMovieJetpackTheme {
-        Greeting("Android")
+        Box{
+            Greeting("Android")
+            Greeting("Antonio")
+        }
     }
 }
