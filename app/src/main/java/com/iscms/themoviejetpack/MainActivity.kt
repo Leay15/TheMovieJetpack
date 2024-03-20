7752a61ece9031b6eac084b7f25664220b454f34
Leay15 <ivan_yael_gp@outlook.com>
2024-03-19 20:31:40 -0600

    Modifier example

--- app/src/main/java/com/iscms/themoviejetpack/MainActivity.kt
+++ app/src/main/java/com/iscms/themoviejetpack/MainActivity.kt
@@ -3,14 +3,25 @@ package com.iscms.themoviejetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.iscms.themoviejetpack.ui.theme.TheMovieJetpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +51,59 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
+fun MediaItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+
+        }
+        Text(text = "Title 1")
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 400, heightDp = 200)
+@Composable
+fun ButtonText() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(
+            text = "Hello world",
+            modifier = Modifier
+                .background(Color.Cyan)
+                .border(2.dp, Color.Blue)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun GreetingPreview() {
     TheMovieJetpackTheme {
-        Box{
-            Greeting("Android")
-            Greeting("Antonio")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Greeting(
+                name = "Android", modifier = Modifier
+                    .background(Color.Gray)
+                    .weight(2f)
+            )
+            Greeting(
+                name = "Antonio", modifier = Modifier
+                    .background(Color.Blue)
+                    .weight(2f)
+            )
+            Greeting(
+                name = "Ivan", modifier = Modifier
+                    .background(Color.Cyan)
+                    .weight(3f)
+            )
         }
     }
 }
